9bd23d4760e1d492237505aed19dd9171b64f77a
chennu998 <hemuchennu529@gmail.com>
2025-01-27 22:15:46 +0530

    added create table file

---
+++ sql_hemant/DDL/create.sql
@@ -0,0 +1,13 @@
+/*create command
+create is a ddl(datadefenitionlanguage) command.
+it is used for creating tables,databases.*/
+
+--create database command
+CREATE DATABASE databasename;
+create database university
+
+--create table command
+create table tablename(column1 datatype,column2 datatype....)
+create table student(id int,name varchar(100),age int,department varchar(100));
+
+
