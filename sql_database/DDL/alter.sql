6c18f779e527e338a6dcc087a267ef7710c823c1
chennu998 <hemuchennu529@gmail.com>
2025-01-28 11:54:49 +0530

    added ddl commands

---
+++ sql_database/DDL/alter.sql
@@ -0,0 +1,22 @@
+/* alter table command
+it is used to change the structure of the table like 
+adding new column,deleting column,changing the names of columns ,datatypes of columns etc.*/
+
+--adding a new column to table 
+alter table table_name add  newcolumn_name datatype;
+alter table student add column email varchar(100)
+
+--changing datatype of a column
+alter table table_name modify column_name new_datatype;
+ALTER TABLE students MODIFY age BIGINT;
+
+
+--deleting a column 
+alter table table_name drop column column_name
+ALTER TABLE students DROP COLUMN age;
+
+
+--rename a column
+alter table table_name rename column  column_name to new_column_name
+ALTER TABLE
+  player RENAME COLUMN jersey_num TO jersey_number;
---
+++ sql_database/DDL/drop.sql
@@ -0,0 +1,8 @@
+/* drop command
+it is used to delete tables,databases*/
+
+--delete table
+drop table colleges
+
+--delete database
+drop database university
